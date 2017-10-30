36049cb0b93b7426bab6343c93586f92006ddc0f
Andrii MICHAK <amichak@e2r7p3.unit.ua>
2017-10-30 21:04:13 +0200

    after test

--- ft_strequ.c
+++ ft_strequ.c
@@ -12,14 +12,16 @@
 
 #include "libft.h"
 
-int		ft_strequ(char const *s1, char const *s2)
+int		ft_strequ(const char *s1, const char *s2)
 {
-	while (*s1 == *s2 && s1 && s2)
+	if (!s1 || !s2)
+		return (0);
+	while (*s1 == *s2 && *s1 && *s2)
 	{
-		if (*s1 == '\0')
-			return (1);
 		s1++;
 		s2++;
 	}
+	if (*s1 == '\0' && *s2 == '\0')
+			return (1);
 	return (0);
 }
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -12,14 +12,19 @@
 
 #include "libft.h"
 
-int		ft_strnequ(char const *s1, char const *s2, size_t n)
+int		ft_strnequ(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 == *s2 && s1 && s2 && n--)
+	if (!s1 || !s2)
+		return (0);
+	while (*s1 == *s2)
 	{
+		n--;
 		if (*s1 == '\0')
 			return (1);
 		s1++;
 		s2++;
 	}
+	if (!n || !*s1 || !*s2)
+		return (1);
 	return (0);
 }
